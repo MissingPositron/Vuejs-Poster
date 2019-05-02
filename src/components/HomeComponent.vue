7fef75202e9d6dc058e43dc8db60886f56f5dbbf
Galileo Liu <Galileo.Liu@wdc.com>
2019-05-02 08:15:18 +0800

    The website for poster collection

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,16 @@
+// HomeComponent.vue
+
+<template>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card car-default">
+        <div class="card-header">Home Component</div>
+        <div class="card-body">I'm the Home Component component.</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
